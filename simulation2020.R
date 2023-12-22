e33acafbac9bd56b4ae9f879c8d2055458039125
Piotr Chlebicki <piochl@st.amu.edu.pl>
2023-12-22 01:13:34 +0100

    Sim results

---
+++ simulation2020.R
@@ -0,0 +1,489 @@
+# coverage 
+library(nonprobsvy)
+library(sampling)
+library(doParallel)
+library(foreach)
+library(tidyverse)
+
+seed_for_sim <- 2023-12-21
+set.seed(seed_for_sim)
+
+N <- 10000
+n_A <- 500
+p <- 50
+KK <- 1
+alpha_vec1 <- c(-2, 1, 1, 1, 1, rep(0, p - 5))
+alpha_vec2 <- c(0, 0, 0, 3, 3, 3, 3, rep(0, p - 7))
+beta_vec <- c(1, 0, 0, 1, 1, 1, 1, rep(0, p - 7))
+
+## generate X
+X <- cbind(
+  "(Intercept)" = 1, 
+  matrix(
+    rnorm(N * (p - 1)), 
+    nrow = N, byrow = TRUE, 
+    dimnames = list(NULL, paste0("X", 1:(p - 1)))
+  )
+)
+X_formula  <- as.formula(paste("~", paste0("X", 1:(p - 1), collapse = " + ")))
+#X_totals <- colSums(X)
+#X_means <- colMeans(X[,-1])
+
+## generate Y
+Y_11 <- as.numeric(X %*% beta_vec) +  rnorm(N) ## OM I: linear model
+Y_12 <- 1 + exp(3*sin(as.numeric(X %*% beta_vec))) + X[, "X5"] + X[, "X6"] + rnorm(N) ## OM II: nonlinear model
+pi_Y_21 <- plogis(as.numeric(X %*% beta_vec)) ## OM III: linear model for binary Y
+pi_Y_22 <- plogis(2 - log((X %*% beta_vec)^2) - 2*X[,"X5"] + 2*X[, "X6"]) ## OM IV: nonlinear model for binary Y
+Y_21 <- rbinom(N, 1, prob = pi_Y_21)
+Y_22 <- rbinom(N, 1, prob = pi_Y_22)
+
+## generate probs
+pi_A <- inclusionprobabilities(0.25 + abs(X[, "X1"]) + 0.03*abs(Y_11), n_A) ## inclusion based on Y_11 only 
+pi_B1 <- plogis(as.numeric(X %*% alpha_vec1)) ## PSM I: linear probability
+
+## generate data
+pop_data <- data.frame(pi_A, Y_11, Y_12, Y_21, Y_22, X[, 2:p])
+head(pop_data)
+
+cores <- parallel::detectCores() - 1
+cl <- makeCluster(cores)
+
+registerDoParallel(cl)
+
+res <- foreach(k=1:7, .combine = rbind,
+               .packages = c("survey", "nonprobsvy", "sampling"),
+               .verbose = TRUE) %dopar% {
+  ## generate sample
+  flag_B1 <- rbinom(N, 1, prob = pi_B1)
+  flag_A <- UPpoisson(pik = pi_A)
+  
+  sample_A_svy <- svydesign(ids = ~ 1, probs = ~ pi_A, pps = "brewer", data = pop_data[flag_A == 1, ])
+  
+  sample_B1 <- pop_data[flag_B1 == 1, ]
+  
+  ## Mass imputation with pmm, yhat-yhat, no var select
+  
+  est_mi_pmm_y11_yhat_yhat <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y12_yhat_yhat <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y21_yhat_yhat <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y22_yhat_yhat <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm"
+  )
+  
+  ## Mass imputation with pmm, yhat-yhat, var select
+  
+  est_mi_pmm_y11_yhat_yhat_sel <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y12_yhat_yhat_sel <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y21_yhat_yhat_sel <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y22_yhat_yhat_sel <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  
+  ## Mass imputation with pmm, yhat-y, no var select
+  
+  est_mi_pmm_y11_yhat_y <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 1),
+    family_outcome = "gaussian",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y12_yhat_y <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 1),
+    family_outcome = "gaussian",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y21_yhat_y <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 1),
+    family_outcome = "binomial",
+    method_outcome = "pmm"
+  )
+  
+  est_mi_pmm_y22_yhat_y <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 1),
+    family_outcome = "binomial",
+    method_outcome = "pmm"
+  )
+  
+  ## Mass imputation with pmm, yhat-y, var select
+  
+  est_mi_pmm_y11_yhat_y_sel <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y12_yhat_y_sel <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "gaussian",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y21_yhat_y_sel <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_pmm_y22_yhat_y_sel <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    control_outcome = controlOut(k = KK, predictive_match = 2),
+    family_outcome = "binomial",
+    method_outcome = "pmm",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  ## Mass imputation with glm, no var select
+  
+  est_mi_glm_y11 <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "gaussian"
+  )
+  
+  est_mi_glm_y12 <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "gaussian"
+  )
+  
+  est_mi_glm_y21 <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "binomial"
+  )
+  
+  est_mi_glm_y22 <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "binomial"
+  )
+  
+  ## Mass imputation with glm, var select
+  
+  est_mi_glm_y11_sel <- nonprob(
+    outcome = as.formula(paste("Y_11 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "gaussian",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_glm_y12_sel <- nonprob(
+    outcome = as.formula(paste("Y_12 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "gaussian",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_glm_y21_sel <- nonprob(
+    outcome = as.formula(paste("Y_21 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "binomial",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  est_mi_glm_y22_sel <- nonprob(
+    outcome = as.formula(paste("Y_22 ~ ", as.character(X_formula)[2])),
+    data = sample_B1,
+    svydesign = sample_A_svy,
+    family_outcome = "binomial",
+    control_inference = controlInf(vars_selection = TRUE)
+  )
+  
+  cbind(
+    # Y11
+    est_mi_pmm_y11_yhat_yhat$output,
+    est_mi_pmm_y11_yhat_yhat$confidence_interval,
+    est_mi_pmm_y11_yhat_yhat_sel$output,
+    est_mi_pmm_y11_yhat_yhat_sel$confidence_interval,
+    est_mi_pmm_y11_yhat_y$output,
+    est_mi_pmm_y11_yhat_y$confidence_interval,
+    est_mi_pmm_y11_yhat_y_sel$output,
+    est_mi_pmm_y11_yhat_y_sel$confidence_interval,
+    est_mi_glm_y11$output,
+    est_mi_glm_y11$confidence_interval,
+    est_mi_glm_y11_sel$output,
+    est_mi_glm_y11_sel$confidence_interval,
+    mean(pop_data$Y_11),
+    # Y12
+    est_mi_pmm_y12_yhat_yhat$output,
+    est_mi_pmm_y12_yhat_yhat$confidence_interval,
+    est_mi_pmm_y12_yhat_yhat_sel$output,
+    est_mi_pmm_y12_yhat_yhat_sel$confidence_interval,
+    est_mi_pmm_y12_yhat_y$output,
+    est_mi_pmm_y12_yhat_y$confidence_interval,
+    est_mi_pmm_y12_yhat_y_sel$output,
+    est_mi_pmm_y12_yhat_y_sel$confidence_interval,
+    est_mi_glm_y12$output,
+    est_mi_glm_y12$confidence_interval,
+    est_mi_glm_y12_sel$output,
+    est_mi_glm_y12_sel$confidence_interval,
+    mean(pop_data$Y_12),
+    # Y21
+    est_mi_pmm_y21_yhat_yhat$output,
+    est_mi_pmm_y21_yhat_yhat$confidence_interval,
+    est_mi_pmm_y21_yhat_yhat_sel$output,
+    est_mi_pmm_y21_yhat_yhat_sel$confidence_interval,
+    est_mi_pmm_y21_yhat_y$output,
+    est_mi_pmm_y21_yhat_y$confidence_interval,
+    est_mi_pmm_y21_yhat_y_sel$output,
+    est_mi_pmm_y21_yhat_y_sel$confidence_interval,
+    est_mi_glm_y21$output,
+    est_mi_glm_y21$confidence_interval,
+    est_mi_glm_y21_sel$output,
+    est_mi_glm_y21_sel$confidence_interval,
+    mean(pop_data$Y_21),
+    # Y22
+    est_mi_pmm_y22_yhat_yhat$output,
+    est_mi_pmm_y22_yhat_yhat$confidence_interval,
+    est_mi_pmm_y22_yhat_yhat_sel$output,
+    est_mi_pmm_y22_yhat_yhat_sel$confidence_interval,
+    est_mi_pmm_y22_yhat_y$output,
+    est_mi_pmm_y22_yhat_y$confidence_interval,
+    est_mi_pmm_y22_yhat_y_sel$output,
+    est_mi_pmm_y22_yhat_y_sel$confidence_interval,
+    est_mi_glm_y22$output,
+    est_mi_glm_y22$confidence_interval,
+    est_mi_glm_y22_sel$output,
+    est_mi_glm_y22_sel$confidence_interval,
+    mean(pop_data$Y_22)
+  )
+}
+
+stopCluster(cl)
+res <- as.data.frame(res)
+
+colnames(res) <- c(
+  "Y_11-est-yhat-yhat-match-no-var-sel-est",
+  "Y_11-est-yhat-yhat-match-no-var-sel-se",
+  "Y_11-est-yhat-yhat-match-no-var-sel-lower",
+  "Y_11-est-yhat-yhat-match-no-var-sel-upper",
+  "Y_11-est-yhat-yhat-match-var-sel-est",
+  "Y_11-est-yhat-yhat-match-var-sel-se",
+  "Y_11-est-yhat-yhat-match-var-sel-lower",
+  "Y_11-est-yhat-yhat-match-var-sel-upper",
+  "Y_11-est-yhat-y-match-no-var-sel-est",
+  "Y_11-est-yhat-y-match-no-var-sel-se",
+  "Y_11-est-yhat-y-match-no-var-sel-lower",
+  "Y_11-est-yhat-y-match-no-var-sel-upper",
+  "Y_11-est-yhat-y-match-var-sel-est",
+  "Y_11-est-yhat-y-match-var-sel-se",
+  "Y_11-est-yhat-y-match-var-sel-lower",
+  "Y_11-est-yhat-y-match-var-sel-upper",
+  "Y_11-est-mi-no-var-sel-est",
+  "Y_11-est-mi-no-var-sel-se",
+  "Y_11-est-mi-no-var-sel-lower",
+  "Y_11-est-mi-no-var-sel-upper",
+  "Y_11-est-mi-var-sel-est",
+  "Y_11-est-mi-var-sel-se",
+  "Y_11-est-mi-var-sel-lower",
+  "Y_11-est-mi-var-sel-upper",
+  "Y_11_true_mean",
+  "Y_12-est-yhat-yhat-match-no-var-sel-est",
+  "Y_12-est-yhat-yhat-match-no-var-sel-se",
+  "Y_12-est-yhat-yhat-match-no-var-sel-lower",
+  "Y_12-est-yhat-yhat-match-no-var-sel-upper",
+  "Y_12-est-yhat-yhat-match-var-sel-est",
+  "Y_12-est-yhat-yhat-match-var-sel-se",
+  "Y_12-est-yhat-yhat-match-var-sel-lower",
+  "Y_12-est-yhat-yhat-match-var-sel-upper",
+  "Y_12-est-yhat-y-match-no-var-sel-est",
+  "Y_12-est-yhat-y-match-no-var-sel-se",
+  "Y_12-est-yhat-y-match-no-var-sel-lower",
+  "Y_12-est-yhat-y-match-no-var-sel-upper",
+  "Y_12-est-yhat-y-match-var-sel-est",
+  "Y_12-est-yhat-y-match-var-sel-se",
+  "Y_12-est-yhat-y-match-var-sel-lower",
+  "Y_12-est-yhat-y-match-var-sel-upper",
+  "Y_12-est-mi-no-var-sel-est",
+  "Y_12-est-mi-no-var-sel-se",
+  "Y_12-est-mi-no-var-sel-lower",
+  "Y_12-est-mi-no-var-sel-upper",
+  "Y_12-est-mi-var-sel-est",
+  "Y_12-est-mi-var-sel-se",
+  "Y_12-est-mi-var-sel-lower",
+  "Y_12-est-mi-var-sel-upper",
+  "Y_12_true_mean",
+  "Y_21-est-yhat-yhat-match-no-var-sel-est",
+  "Y_21-est-yhat-yhat-match-no-var-sel-se",
+  "Y_21-est-yhat-yhat-match-no-var-sel-lower",
+  "Y_21-est-yhat-yhat-match-no-var-sel-upper",
+  "Y_21-est-yhat-yhat-match-var-sel-est",
+  "Y_21-est-yhat-yhat-match-var-sel-se",
+  "Y_21-est-yhat-yhat-match-var-sel-lower",
+  "Y_21-est-yhat-yhat-match-var-sel-upper",
+  "Y_21-est-yhat-y-match-no-var-sel-est",
+  "Y_21-est-yhat-y-match-no-var-sel-se",
+  "Y_21-est-yhat-y-match-no-var-sel-lower",
+  "Y_21-est-yhat-y-match-no-var-sel-upper",
+  "Y_21-est-yhat-y-match-var-sel-est",
+  "Y_21-est-yhat-y-match-var-sel-se",
+  "Y_21-est-yhat-y-match-var-sel-lower",
+  "Y_21-est-yhat-y-match-var-sel-upper",
+  "Y_21-est-mi-no-var-sel-est",
+  "Y_21-est-mi-no-var-sel-se",
+  "Y_21-est-mi-no-var-sel-lower",
+  "Y_21-est-mi-no-var-sel-upper",
+  "Y_21-est-mi-var-sel-est",
+  "Y_21-est-mi-var-sel-se",
+  "Y_21-est-mi-var-sel-lower",
+  "Y_21-est-mi-var-sel-upper",
+  "Y_21_true_mean",
+  "Y_22-est-yhat-yhat-match-no-var-sel-est",
+  "Y_22-est-yhat-yhat-match-no-var-sel-se",
+  "Y_22-est-yhat-yhat-match-no-var-sel-lower",
+  "Y_22-est-yhat-yhat-match-no-var-sel-upper",
+  "Y_22-est-yhat-yhat-match-var-sel-est",
+  "Y_22-est-yhat-yhat-match-var-sel-se",
+  "Y_22-est-yhat-yhat-match-var-sel-lower",
+  "Y_22-est-yhat-yhat-match-var-sel-upper",
+  "Y_22-est-yhat-y-match-no-var-sel-est",
+  "Y_22-est-yhat-y-match-no-var-sel-se",
+  "Y_22-est-yhat-y-match-no-var-sel-lower",
+  "Y_22-est-yhat-y-match-no-var-sel-upper",
+  "Y_22-est-yhat-y-match-var-sel-est",
+  "Y_22-est-yhat-y-match-var-sel-se",
+  "Y_22-est-yhat-y-match-var-sel-lower",
+  "Y_22-est-yhat-y-match-var-sel-upper",
+  "Y_22-est-mi-no-var-sel-est",
+  "Y_22-est-mi-no-var-sel-se",
+  "Y_22-est-mi-no-var-sel-lower",
+  "Y_22-est-mi-no-var-sel-upper",
+  "Y_22-est-mi-var-sel-est",
+  "Y_22-est-mi-var-sel-se",
+  "Y_22-est-mi-var-sel-lower",
+  "Y_22-est-mi-var-sel-upper",
+  "Y_22_true_mean"
+)
+
+df <- res |> 
+  pivot_longer(cols = everything(), names_to = "column", values_to = "value") |>
+  mutate(
+    y_name   = str_sub(column, start = 1, end = 4),
+    est_name = ifelse(
+      !is.na(str_match(column, "yhat-yhat")),
+      ifelse(
+        !is.na(str_match(column, "no-var-sel")),
+        "yhat-yhat-no-var-sel",
+        "yhat-yhat-var-sel"
+      ),
+      ifelse(
+        is.na(str_match(column, "mi")),
+        ifelse(
+          !is.na(str_match(column, "no-var-sel")),
+          "yhat-y-no-var-sel",
+          "yhat-y-var-sel"
+        ),
+        ifelse(
+          !is.na(str_match(column, "no-var-sel")),
+          "glm-no-var-sel",
+          "glm-var-sel"
+        )
+      )
+    ) |> as.character(),
+    column = str_extract(column, "(mean|upper|lower|true)")
+  ) |> 
+  mutate(column = ifelse(is.na(column), "se", column)) |>
+  pivot_wider(names_from = column,
+              values_from = value)
+
+
+
+saveRDS(res, file = "results/yang2020-pmm-500-sims.rds")
+
