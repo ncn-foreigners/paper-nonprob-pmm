830968def7cccade3182cf481b9cdead7861b4b6
Piotr Chlebicki <piochl@st.amu.edu.pl>
2024-01-03 13:08:16 +0100

    update sim to check properties under violation of P(delta = 1|X) > 0 a.s.

--- sim.R
+++ sim.R
@@ -1,8 +1,8 @@
 # instalacja z tego branchu
 #remotes::install_github("ncn-foreigners/nonprobsvy@dev_2")
 library(nonprobsvy)
-library(doParallel)
-library(foreach)
+library(doSNOW)
+library(progress)
 
 set.seed(123)
 
@@ -29,17 +29,29 @@ population <- data.frame(
 cl <- makeCluster(cores)
 clusterExport(cl, c("N", "n"))
 
-registerDoParallel(cl)
+registerDoSNOW(cl)
+
+pb <- progress_bar$new(total = sims)
+
+opts <- list(progress = \(n) pb$tick())
 
 res <- foreach(k=1:sims, .combine = rbind,
                .packages = c("survey", "nonprobsvy"),
-               .verbose = TRUE) %dopar% {
+               .options.snow = opts) %dopar% {
   flag_srs <- rbinom(n = N, size = 1, prob = n / N)
-  flag_bd1 <- rbinom(n = N, size = 1, prob = p1)
+  flag_bd1 <- c(rbinom(n = 1:(N - 10000), size = 1, prob = p1), rep(0, 10000))
   base_w_bd <- N/sum(flag_bd1)
   sample_prob <- svydesign(ids= ~1, weights = ~ base_w_srs,
                            data = subset(population, flag_srs == 1))
 
+  glm1 <- nonprob(
+    outcome = y1 ~ x1 + x2,
+    data = population[flag_bd1 == 1, , drop = FALSE],
+    svydesign = sample_prob,
+    pop_size = N,
+    family_outcome = "gaussian"
+  )
+  
   pmm1 <- nonprob(
     outcome = y1 ~ x1 + x2,
     data = population[flag_bd1 == 1, , drop = FALSE],
@@ -80,6 +92,14 @@ res <- foreach(k=1:sims, .combine = rbind,
     family_outcome = "gaussian",
     control_outcome = controlOut(k = KK, predictive_match = 2),
     control_inference = controlInf(pmm_exact_se = TRUE)
+  )
+  
+  glm2 <- nonprob(
+    outcome = y2 ~ x1 + x2,
+    data = population[flag_bd1 == 1, , drop = FALSE],
+    svydesign = sample_prob,
+    pop_size = N,
+    family_outcome = "gaussian"
   )
 
   pmm2 <- nonprob(
@@ -130,6 +150,7 @@ res <- foreach(k=1:sims, .combine = rbind,
   cbind(
     pmm1$output$mean, pmm1.1$output$mean,
     pmm1.2$output$mean, pmm1.3$output$mean,
+    glm1$output$mean,
     pmm1$confidence_interval[, 1] < mean(population$y1) &
       mean(population$y1) < pmm1$confidence_interval[, 2],
     pmm1.1$confidence_interval[, 1] < mean(population$y1) &
@@ -138,9 +159,12 @@ res <- foreach(k=1:sims, .combine = rbind,
       mean(population$y1) < pmm1.2$confidence_interval[, 2],
     pmm1.3$confidence_interval[, 1] < mean(population$y1) &
       mean(population$y1) < pmm1.3$confidence_interval[, 2],
+    glm1$confidence_interval[, 1] < mean(population$y1) &
+      mean(population$y1) < glm1$confidence_interval[, 2],
     mean(population$y1),
     pmm2$output$mean, pmm2.1$output$mean,
     pmm2.2$output$mean, pmm2.3$output$mean,
+    glm2$output$mean,
     pmm2$confidence_interval[, 1] < mean(population$y2) &
       mean(population$y2) < pmm2$confidence_interval[, 2],
     pmm2.1$confidence_interval[, 1] < mean(population$y2) &
@@ -149,11 +173,14 @@ res <- foreach(k=1:sims, .combine = rbind,
       mean(population$y2) < pmm2.2$confidence_interval[, 2],
     pmm2.3$confidence_interval[, 1] < mean(population$y2) &
       mean(population$y2) < pmm2.3$confidence_interval[, 2],
+    glm2$confidence_interval[, 1] < mean(population$y2) &
+      mean(population$y2) < glm2$confidence_interval[, 2],
     mean(population$y2),
     pmm1$output$SE, pmm1.1$output$SE,
     pmm1.2$output$SE, pmm1.3$output$SE,
-    pmm2$output$SE, pmm2.1$output$SE,
-    pmm2.2$output$SE, pmm2.3$output$SE
+    glm1$output$SE, pmm2$output$SE, 
+    pmm2.1$output$SE, pmm2.2$output$SE, 
+    pmm2.3$output$SE, glm2$output$SE
   )
 }
 
@@ -163,110 +190,132 @@ colnames(res) <- c(
   "sample 1 - yhat - yhat match",#2
   "sample 1 - yhat - y match - third term included",#3
   "sample 1 - yhat - yhat match - third term included",#4
-  "sample 1 - yhat - y match - coverage",#5
-  "sample 1 - yhat - yhat match - coverage",#6
-  "sample 1 - yhat - y match - coverage - third term included",#7
-  "sample 1 - yhat - yhat match - coverage - third term included",#8
-  "sample 1 - mean of y",#9
-  "sample 2 - yhat - y match",#10
-  "sample 2 - yhat - yhat match",#11
-  "sample 2 - yhat - y match - third term included",#12
-  "sample 2 - yhat - yhat match - third term included",#13
-  "sample 2 - yhat - y match - coverage",#14
-  "sample 2 - yhat - yhat match - coverage",#15
-  "sample 2 - yhat - y match - coverage - third term included",#16
-  "sample 2 - yhat - yhat match - coverage - third term included",#17
-  "sample 2 - mean of y",#18
-  "sample 1 - est se yhat - y",#19
-  "sample 1 - est se yhat - yhat",#20
-  "sample 1 - est se yhat - y - third term included",#21
-  "sample 1 - est se yhat - yhat - third term included",#22
-  "sample 2 - est se yhat - y",#23
-  "sample 2 - est se yhat - yhat",#24
-  "sample 2 - est se yhat - y - third term included",#25
-  "sample 2 - est se yhat - yhat - third term included"#26
+  "sample 1 - glm",#5
+  "sample 1 - yhat - y match - coverage",#6
+  "sample 1 - yhat - yhat match - coverage",#7
+  "sample 1 - yhat - y match - coverage - third term included",#8
+  "sample 1 - yhat - yhat match - coverage - third term included",#9
+  "sample 1 - glm - coverage",#10
+  "sample 1 - mean of y",#11
+  "sample 2 - yhat - y match",#12
+  "sample 2 - yhat - yhat match",#13
+  "sample 2 - yhat - y match - third term included",#14
+  "sample 2 - yhat - yhat match - third term included",#15
+  "sample 2 - glm",#16
+  "sample 2 - yhat - y match - coverage",#17
+  "sample 2 - yhat - yhat match - coverage",#18
+  "sample 2 - yhat - y match - coverage - third term included",#19
+  "sample 2 - yhat - yhat match - coverage - third term included",#20
+  "sample 2 - glm - coverage",#21
+  "sample 2 - mean of y",#22
+  "sample 1 - est se yhat - y",#23
+  "sample 1 - est se yhat - yhat",#24
+  "sample 1 - est se yhat - y - third term included",#25
+  "sample 1 - est se yhat - yhat - third term included",#26
+  "sample 1 - est se glm",#27
+  "sample 2 - est se yhat - y",#28
+  "sample 2 - est se yhat - yhat",#29
+  "sample 2 - est se yhat - y - third term included",#30
+  "sample 2 - est se yhat - yhat - third term included",#31
+  "sample 2 - est se glm"#32
 )
 
 df <- data.frame(
   bias = c(
-    mean(res[,  1] - res[,  9]),
-    mean(res[,  2] - res[,  9]),
-    mean(res[,  3] - res[,  9]),
-    mean(res[,  4] - res[,  9]),
-    mean(res[, 10] - res[, 18]),
-    mean(res[, 11] - res[, 18]),
-    mean(res[, 12] - res[, 18]),
-    mean(res[, 13] - res[, 18])
+    mean(res[,  1] - res[, 11]),
+    mean(res[,  2] - res[, 11]),
+    mean(res[,  3] - res[, 11]),
+    mean(res[,  4] - res[, 11]),
+    mean(res[,  5] - res[, 11]),
+    mean(res[, 12] - res[, 22]),
+    mean(res[, 13] - res[, 22]),
+    mean(res[, 14] - res[, 22]),
+    mean(res[, 15] - res[, 22]),
+    mean(res[, 16] - res[, 22])
   ),
   mse = c(
-    mean((res[,  1] - res[,  9]) ^ 2),
-    mean((res[,  2] - res[,  9]) ^ 2),
-    mean((res[,  3] - res[,  9]) ^ 2),
-    mean((res[,  4] - res[,  9]) ^ 2),
-    mean((res[, 10] - res[, 18]) ^ 2),
-    mean((res[, 11] - res[, 18]) ^ 2),
-    mean((res[, 12] - res[, 18]) ^ 2),
-    mean((res[, 13] - res[, 18]) ^ 2)
+    mean((res[,  1] - res[, 11]) ^ 2),
+    mean((res[,  2] - res[, 11]) ^ 2),
+    mean((res[,  3] - res[, 11]) ^ 2),
+    mean((res[,  4] - res[, 11]) ^ 2),
+    mean((res[,  5] - res[, 11]) ^ 2),
+    mean((res[, 12] - res[, 22]) ^ 2),
+    mean((res[, 13] - res[, 22]) ^ 2),
+    mean((res[, 14] - res[, 22]) ^ 2),
+    mean((res[, 15] - res[, 22]) ^ 2),
+    mean((res[, 16] - res[, 22]) ^ 2)
   ),
   mae = c(
-    mean(abs(res[,  1] - res[,  9])),
-    mean(abs(res[,  2] - res[,  9])),
-    mean(abs(res[,  3] - res[,  9])),
-    mean(abs(res[,  4] - res[,  9])),
-    mean(abs(res[, 10] - res[, 18])),
-    mean(abs(res[, 11] - res[, 18])),
-    mean(abs(res[, 12] - res[, 18])),
-    mean(abs(res[, 13] - res[, 18]))
+    mean(abs(res[,  1] - res[, 11])),
+    mean(abs(res[,  2] - res[, 11])),
+    mean(abs(res[,  3] - res[, 11])),
+    mean(abs(res[,  4] - res[, 11])),
+    mean(abs(res[,  5] - res[, 11])),
+    mean(abs(res[, 12] - res[, 22])),
+    mean(abs(res[, 13] - res[, 22])),
+    mean(abs(res[, 14] - res[, 22])),
+    mean(abs(res[, 15] - res[, 22])),
+    mean(abs(res[, 16] - res[, 22]))
   ),
-  variance = c(
-    var(res[,  1] - res[,  9]),
-    var(res[,  2] - res[,  9]),
-    var(res[,  3] - res[,  9]),
-    var(res[,  4] - res[,  9]),
-    var(res[, 10] - res[, 18]),
-    var(res[, 11] - res[, 18]),
-    var(res[, 12] - res[, 18]),
-    var(res[, 13] - res[, 18])
+  error_variance = c(
+    var(res[,  1] - res[, 11]),
+    var(res[,  2] - res[, 11]),
+    var(res[,  3] - res[, 11]),
+    var(res[,  4] - res[, 11]),
+    var(res[,  5] - res[, 11]),
+    var(res[, 12] - res[, 22]),
+    var(res[, 13] - res[, 22]),
+    var(res[, 14] - res[, 22]),
+    var(res[, 15] - res[, 22]),
+    var(res[, 16] - res[, 22])
   ),
-  se = c(
-    sd(res[,  1] - res[,  9]),
-    sd(res[,  2] - res[,  9]),
-    sd(res[,  3] - res[,  9]),
-    sd(res[,  4] - res[,  9]),
-    sd(res[, 10] - res[, 18]),
-    sd(res[, 11] - res[, 18]),
-    sd(res[, 12] - res[, 18]),
-    sd(res[, 13] - res[, 18])
+  eoor_se = c(
+    sd(res[,  1] - res[, 11]),
+    sd(res[,  2] - res[, 11]),
+    sd(res[,  3] - res[, 11]),
+    sd(res[,  4] - res[, 11]),
+    sd(res[,  5] - res[, 11]),
+    sd(res[, 12] - res[, 22]),
+    sd(res[, 13] - res[, 22]),
+    sd(res[, 14] - res[, 22]),
+    sd(res[, 15] - res[, 22]),
+    sd(res[, 16] - res[, 22])
   ),
   mean_se_est = c(
-    mean(res[, 19]),
-    mean(res[, 20]),
-    mean(res[, 21]),
-    mean(res[, 22]),
     mean(res[, 23]),
     mean(res[, 24]),
     mean(res[, 25]),
-    mean(res[, 26])
+    mean(res[, 26]),
+    mean(res[, 27]),
+    mean(res[, 28]),
+    mean(res[, 29]),
+    mean(res[, 30]),
+    mean(res[, 31]),
+    mean(res[, 32])
   ),
   coverage = c(
-    mean(res[,  5]),
     mean(res[,  6]),
     mean(res[,  7]),
     mean(res[,  8]),
-    mean(res[, 14]),
-    mean(res[, 15]),
-    mean(res[, 16]),
-    mean(res[, 17])
+    mean(res[,  9]),
+    mean(res[, 10]),
+    mean(res[, 17]),
+    mean(res[, 18]),
+    mean(res[, 19]),
+    mean(res[, 20]),
+    mean(res[, 21])
   ),
   row.names = c(
     "sample 1 - yhat - y match",
     "sample 1 - yhat - yhat match",
     "sample 1 - yhat - y match - third term included",
     "sample 1 - yhat - yhat match - third term included",
+    "sample 1 - glm",
     "sample 2 - yhat - y match",
     "sample 2 - yhat - yhat match",
     "sample 2 - yhat - y match - third term included",
-    "sample 2 - yhat - yhat match - third term included"
+    "sample 2 - yhat - yhat match - third term included",
+    "sample 2 - yhat - glm"
   )
 )
 
